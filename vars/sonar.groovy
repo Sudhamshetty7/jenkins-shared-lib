7a57d6b43ed9d94605d65c9890c2ee6a5df21c8b
Sudhamshetty7 <154966370+Sudhamshetty7@users.noreply.github.com>
2024-01-22 20:27:38 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,6 +1,7 @@
-def call(){
-    echo "SonarQube Analysis"
-    sh '''
-      mvn clean package sonar:sonar
-    '''
-}
+def call() {
+       // Your SonarQube scan steps go here
+      // sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://http://13.232.78.101:9000 -Dsonar.java.binaries=target/classes'
+       sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://http://13.232.78.101:9000 -Dsonar=sonar.exclusions*/.java'
+   }
+
+return this
